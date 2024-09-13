69b8f4f662c373628fe8c4acb3a90099d9f0561f
BetteriX <szabozsolt430@gmail.com>
2024-09-13 15:41:58 +0200

    1_gyakorlat

---
+++ Gyakorlat/01/hello/Main.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static int two(){
+        return 2;
+    }
+    
+    public static void main(String[] args){
+        int z = 100_000;
+
+        long y = 123456789012L;
+
+        float vml = 6.78f;
+
+        double pi = 3.14f;
+
+        String text = "Java 21 rulz";
+
+        System.out.println(text);
+        
+    }
+}
