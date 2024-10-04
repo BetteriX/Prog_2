2c39bce7ab23e1e331f117914d0566e3ef54cc24
BetteriX <szabozsolt430@gmail.com>
2024-10-04 15:37:32 +0200

    4_ora_gyak

---
+++ Gyakorlat/04/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //List<Integer> result1 = PyUtils.range(20);
+        //List<Integer> result1 = PyUtils.range(5,20);
+        //List<Integer> result1 = PyUtils.range(20,5,2);
+
+        List<Integer> szamok = new ArrayList<>();
+
+        szamok.add(2);
+        szamok.add(3);
+        szamok.add(5);
+
+        //System.out.println(szamok);
+
+        //kiir(szamok);
+
+        List<Integer> copy = new ArrayList<>();
+
+        co}
+
+    public static void kiir(List<Integer> lista) {
+        System.out.println(lista);
+    }
+}
---
+++ Gyakorlat/04/sztring.java
@@ -0,0 +1,9 @@
+public class sztring {
+    public static void main(String[] args) {
+        String s = "Java 21";
+
+        for (char c : s.toCharArray()) {
+            System.out.println(c);
+        }
+    }
+}
