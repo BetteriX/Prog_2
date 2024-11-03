d3208fe0ea6efd1717eaf71eb33fded4e6a3c72e
BetteriX <szabozsolt430@gmail.com>
2024-11-03 13:28:53 +0100

    teszt

--- !1.ZH_gyak/teszt.java
+++ !1.ZH_gyak/teszt.java
@@ -5,5 +5,9 @@ public class teszt {
         StringBuilder sb = new StringBuilder();
 
         System.out.println(language.toLowerCase().charAt(0));
+        String szoveg_1 = "test";
+        String szoveg_2 = "testt";
+
+        System.out.println(szoveg_1.equals(szoveg_2));
     }
 }
