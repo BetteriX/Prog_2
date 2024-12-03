2f1015c05056eebbb589802f765e009f5716a07b
BetteriX <szabozsolt430@gmail.com>
2024-12-03 09:42:48 +0100

    12_ora

---
+++ 12/AoC2024/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AoC2024
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {   
+        }
+    }
+}
---
+++ 12/Mezogazdasagi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Mezogazdasagi
+{
+    internal class Program
+    {
+        private static Random r = new Random();
+
+        public static void Main(string[] args)
+        {
+            
+            Console.Write("Add meg a búza mennyiségét: ");
+            int suly = int.Parse(Console.ReadLine());
+            
+            int szorzo = r.Next(5, 15 + 1);
+
+            String evFajta = Ev(szorzo);
+            Console.WriteLine($"A {suly} tonna: " + evFajta);
+        }
+
+        private static string Ev(int num)
+        {
+            if (num >= 5 && num <= 8)
+            {
+                return "Átlag allati";
+            }
+            else if (num >= 9 && num <= 12)
+            {
+                return "Átlagos év";
+            }
+            else
+            {
+                return "Átlag felleti";
+            }
+        }
+    }
+}
